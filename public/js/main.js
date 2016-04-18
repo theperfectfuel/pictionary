91ae3a6dd8fc4729836770f08a840ae7601057f2
theperfectfuel <robert@theperfectfuel.com>
2016-04-18 00:14:51 -0700

    Finished Only draw on mousedown issue

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 var socket = io();
+var drawing;
 
 var pictionary = function() {
 	var canvas, context;
@@ -13,17 +14,34 @@ var pictionary = function() {
 	context = canvas[0].getContext('2d');
 	canvas[0].width = canvas[0].offsetWidth;
 	canvas[0].height = canvas[0].offsetHeight;
+
+	// If mouse is held down run the mousemove code
+
+	canvas.on('mousedown', function() {
+		drawing = true;
+		console.log(drawing);
+	});
+
+	canvas.on('mouseup', function() {
+		drawing = false;
+		console.log(drawing);
+	});
+
 	canvas.on('mousemove', function(event) {
 		var offset = canvas.offset();
 		var position = {
 			x: event.pageX - offset.left,
 			y: event.pageY - offset.top
 		};
-		draw(position);
 
-		socket.emit('draw', position);
-		socket.on('draw', draw);
+		if (drawing) {
+			draw(position);
+
+			socket.emit('draw', position);
+			socket.on('draw', draw);
+		}
 	});
+
 };
 
 $(document).ready(function() {
